afaea231a938a736d91e47106f59545890e701bb
Marie-Fe <mariefepingping00@gmail.com>
2023-02-08 18:11:54 +0800

    Commit

---
+++ app/src/main/java/com/android/activity01_a/Act01-A.kt
@@ -0,0 +1,24 @@
+package com.android.activity01_a
+
+fun main() {
+
+    var students = listOf("Alexander Chou, Vic Recopuerto Amable, John Chris Bolan," +
+            " Aziza Villanueva San Buenaventura,andrew bustos, Rizwan Caser, Condino Mark Andrei," +
+            " Laurence De Dios, Jerome Elenzano, Noven Rey M. Gumnad, Hanz Bradford Jaramillo, Junzon Velasco," +
+            " ZR Lopez, Victor Paolo Luzares, Ryan Malacao, Phil Seong Manabat, Ray Andrew Manila," +
+            " Dad Ivan Navidad, Marie Fe Y. Dela Peña, Arthur Aldrin Ramos, Emmanuel De Los Reyes," +
+            " Gretchen Roque, Rey Brezuela Saliot, Jeffrey Saltiga, Rene Galo Tajos, MARK TALIMAN," +
+            " Jother John Tirador, Joe Mari Pasicolan Ubay, Vince Melmar, Ybañez, Raffy Yalung")
+
+    for (x in students) {
+        println(x)
+    }
+
+  //  println("Updates about the class.")
+  //  var updates = readLine()
+
+  //  println("Progress of student[]: ")
+  //  var progress = readLine()
+
+
+}
---
+++ app/src/main/java/com/android/activity01_a/Act01-B.kt
@@ -0,0 +1,13 @@
+package com.android.activity01_a
+
+fun main() {
+
+    var library = listOf("books, board games, comics, newspaper")
+
+    var books = "portable, compact and convenient print media"
+    var boardgames = "play by moving small objects on a board "
+    var comics = "image style, world-image and series of adjacent drawn images"
+    var newspaper = "informative print media"
+
+    println(library)
+}
