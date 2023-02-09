a498990ced90a3ae4b69e63147538c1c13e13b9a
Marie-Fe <mariefepingping00@gmail.com>
2023-02-09 19:51:44 +0800

    Commit

--- app/src/main/java/com/android/activity01_a/Act01-A.kt
+++ app/src/main/java/com/android/activity01_a/Act01-A.kt
@@ -1,24 +1,76 @@
 package com.android.activity01_a
 
+
 fun main() {
 
-    var students = listOf("Alexander Chou, Vic Recopuerto Amable, John Chris Bolan," +
-            " Aziza Villanueva San Buenaventura,andrew bustos, Rizwan Caser, Condino Mark Andrei," +
-            " Laurence De Dios, Jerome Elenzano, Noven Rey M. Gumnad, Hanz Bradford Jaramillo, Junzon Velasco," +
-            " ZR Lopez, Victor Paolo Luzares, Ryan Malacao, Phil Seong Manabat, Ray Andrew Manila," +
-            " Dad Ivan Navidad, Marie Fe Y. Dela Peña, Arthur Aldrin Ramos, Emmanuel De Los Reyes," +
-            " Gretchen Roque, Rey Brezuela Saliot, Jeffrey Saltiga, Rene Galo Tajos, MARK TALIMAN," +
-            " Jother John Tirador, Joe Mari Pasicolan Ubay, Vince Melmar, Ybañez, Raffy Yalung")
-
+    // for list of students
+    var students = listOf(
+        "Alexander Chou",
+        "Vic Recopuerto Amable",
+        "John Chris Bolan",
+        "Aziza Villanueva San Buenaventura",
+        "andrew bustos",
+        "Condino Mark Andrei",
+        "Laurence De Dios",
+        "Jerome Elenzano",
+        "Noven Rey M. Gumnad",
+        "Hanz Bradford Jaramillo",
+        "Junzon Velasco",
+        "ZR Lopez",
+        "Victor Paolo Luzares",
+        "Ryan Malacao",
+        "Phil Seong Manabat",
+        "Ray Andrew Manila",
+        "Dad Ivan Navidad",
+        "Marie Fe Y. Dela Peña",
+        "Arthur Aldrin Ramos",
+        "Emmanuel De Los Reyes",
+        "Gretchen Roque",
+        "Rey Brezuela Saliot",
+        "Jeffrey Saltiga",
+        "Rene Galo Tajos",
+        "MARK TALIMAN",
+        "Jother John Tirador",
+        "Joe Mari Pasicolan Ubay",
+        "Vince Melmar Ybañez",
+        "Raffy Yalung"
+    )
+    println("Students of MD3P:")
     for (x in students) {
         println(x)
     }
 
-  //  println("Updates about the class.")
-  //  var updates = readLine()
+    //Update in the class
+    println("Any updates regarding the class? Y or N:")
+    val update = readLine()
+    //Answerable by string Y and N (not a char)
+    val y = "Y"
+    val n = "N"
+    if (update == y) {
+        println("Update: ")
+        var up = readLine()
+        println(up)
+    } else if (update == n) {
+        println("None")
+    }
 
-  //  println("Progress of student[]: ")
-  //  var progress = readLine()
+    println("Name: ")
+    var progress = readLine()
+    println("Rate progress of student from 1 - 5:")
+    var prorate = readLine()!!.toInt()
+    if (prorate <= 2) {
+        println("The student $progress has a slow progress in the class, need to catch up.")
+    } else if (prorate >= 3) {
+        println("The student $progress is active and passed.")
+    }
 
+
+
+
+  /*   //Nwwithin the [] is an index within 0-
+     println("Progress of $students[3]: ")
+     var progress = readLine()
+     println("Student $students[Any?] $progress")
+ */
 
 }
