095fd46c9fa7cc9266f46c0919b450215e1d8200
Marie-Fe <mariefepingping00@gmail.com>
2023-03-14 17:33:10 +0800

    Commit

--- app/src/main/java/com/android/activity01_a/Activity01D.kt
+++ app/src/main/java/com/android/activity01_a/Activity01D.kt
@@ -1,40 +1,37 @@
 package com.android.activity01_a
-
-import androidx.lifecycle.viewmodel.CreationExtras
-
+import java.util.*
 
 fun main() {
-    println("Input 5 monetary amount:")
+    val scanner = Scanner(System.`in`)
+    val amounts = mutableListOf<Double>()
 
+    // Ask user to input 5 monetary amounts
+    for (i in 1..5) {
+        print("Enter amount $i: ")
+        val amount = readLine()!!.toDouble()
+        amounts.add(amount)
+    }
 
-        print("Input first number:")
-        val a = readLine()!!.toDouble()
-        print("Input second number:")
-        val b = readLine()!!.toDouble()
-        print("Input third number:")
-        val c = readLine()!!.toDouble()
-        print("Input fourth number:")
-        val d = readLine()!!.toDouble()
-        print("Input fifth number:")
-        val e = readLine()!!.toDouble()
+    // Ask user to input divisor with conditions
+    var divisor: Int? = null
+    while (divisor == null) {
+        print("Divide the total by: ")
+        val input = scanner.nextLine()
+        try {
+            divisor = input.toInt()
+            if (divisor <= 0) {
+                throw NumberFormatException()
+            }
+        } catch (e: NumberFormatException) {
+            println("Invalid input. Please enter a positive integer or number.")
+        }
+    }
 
+    // Calculate total and divisor
+    val total = amounts.sum()
+    val quotient = total / divisor
 
-        print("Divide by how many?")
-        val f = readLine()!!.toInt()
-
-        val result = a + b + c + d + e
-        val divide = result / f
-        println("Result: $divide")
-
-
+    // Display results
+    println("Total amount: $total")
+    println("Quotient: $quotient")
 }
-
-
-
-
-
-
-
-
-
-
